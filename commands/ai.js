e4435afc98a3ba9f9d7588aabedd3a59043e5822
Harukai21 <allenbilly2003@gmail.com>
2024-10-08 22:32:57 +0800

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -13,7 +13,8 @@ const MAX_MESSAGE_LENGTH = 2000; // Facebook Messenger message limit
 
 module.exports = {
   name: 'ai',
-  description: 'Listens to any incoming messages, no need for a command name or prefix.',
+  description: 'Listens to any incoming messages',
+  usage: 'just put a message',
   author: 'Biru',
 
   async execute(senderId, messageText, pageAccessToken, sendMessage, messageType = 'text', attachment = null) {
