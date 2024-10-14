8a1745afd63bbc705c70a01ea84987c6ad6f3bb8
Harukai21 <allenbilly2003@gmail.com>
2024-10-14 17:34:30 +0800

    Update autopost.js

--- utils/autopost.js
+++ utils/autopost.js
@@ -11,34 +11,17 @@ module.exports.startAutoPost = function(api) {
                 const response = await axios.get("https://api.popcat.xyz/pickuplines");
                 const pickupLine = response.data.pickupline;
 
-                const message = `“${pickupLine}”`;  // Direct pickup line, no conversion
+                const message = `“${pickupLine}”`;
 
                 const formData = {
-                    input: {
-                        composer_entry_point: "inline_composer",
-                        composer_source_surface: "timeline",
-                        idempotence_token: `${Date.now()}_FEED`,
-                        source: "WWW",
-                        message: {
-                            text: message,
-                        },
-                        audience: {
-                            privacy: {
-                                base_state: "EVERYONE",
-                            },
-                        },
-                        actor_id: api.getCurrentUserID(),
-                    },
+                    message: message,
                 };
 
                 await api.httpPost(
-                    "https://www.facebook.com/api/graphql/",
+                    `https://graph.facebook.com/v12.0/me/feed`, // Updated Graph API URL
                     {
-                        av: api.getCurrentUserID(),
-                        fb_api_req_friendly_name: "ComposerStoryCreateMutation",
-                        fb_api_caller_class: "RelayModern",
-                        doc_id: "7711610262190099",
-                        variables: JSON.stringify(formData),
+                        message: message,
+                        access_token: api.getCurrentUserID()
                     }
                 );
 
