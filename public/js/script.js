d3fb0069a9cefb5fc7f24a18910ca154167df563
Harukai21 <allenbilly2003@gmail.com>
2024-10-21 01:50:32 +0800

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('light-mode');
+
+  // Change the icon based on the mode
+  const modeIcon = document.getElementById('mode-icon');
+  if (document.body.classList.contains('light-mode')) {
+      modeIcon.src = 'assets/moon-icon.png';  // Change to moon icon for dark mode
+  } else {
+      modeIcon.src = 'assets/sun-icon.png';   // Change back to sun icon for light mode
+  }
+}
+
+function redirectToFacebook() {
+  window.location.href = 'https://www.facebook.com/BiruAren'; // Replace with your Facebook link
+}
+
