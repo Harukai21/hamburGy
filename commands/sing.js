8060ddfa9d5be71e6ee07df7538753590004a57f
Harukai21 <allenbilly2003@gmail.com>
2024-10-26 20:30:23 +0800

    Update sing.js

--- commands/sing.js
+++ commands/sing.js
@@ -83,7 +83,15 @@ async function downloadVideoAudio(videoUrl, senderId, sendMessage, pageAccessTok
 
     console.log("Downloading from URL:", downloadLink);
 
-    const response = await axios.get(downloadLink, { responseType: "stream" });
+    const response = await axios.get(downloadLink, {
+      responseType: "stream",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+        "Referer": "https://ytbsave.com/",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+      },
+    });
+
     response.data.pipe(writer);
 
     await new Promise((resolve, reject) => {
