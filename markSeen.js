ba74b9dc4c9fdecfa66757958d452d0d0310e34d
Harukai21 <allenbilly2003@gmail.com>
2024-10-26 23:31:09 +0800

    Update markSeen.js

--- markSeen.js
+++ markSeen.js
@@ -1,10 +1,13 @@
 // markSeen.js
 
-const { sendMessage } = require('./handles/sendMessage');
+const axios = require('axios');
 
 async function markSeen(senderId, pageAccessToken, retries = 3) {
   try {
-    await sendMessage(senderId, { sender_action: 'mark_seen' }, pageAccessToken);
+    await axios.post(`https://graph.facebook.com/v21.0/me/messages?access_token=${pageAccessToken}`, {
+      recipient: { id: senderId },
+      sender_action: 'mark_seen'
+    });
   } catch (error) {
     console.error(`Error marking as seen:`, error);
     if (retries > 0 && (error.code === 'ETIMEDOUT' || error.code === 'ENETUNREACH')) {
