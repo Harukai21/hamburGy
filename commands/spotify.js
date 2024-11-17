f60ca4b2795c678f10cd6e63549146193cb2ec31
Harukai21 <allenbilly2003@gmail.com>
2024-11-17 19:04:11 +0800

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -12,15 +12,23 @@ module.exports = {
       const apiUrl = `https://vneerapi.onrender.com/spotify?song=${encodeURIComponent(query)}`;
       const response = await axios.get(apiUrl);
 
-      const trackName = response.data.track;
-      const artistName = response.data.artist;
-      const spotifyLink = response.data.spotify_url;
-      const downloadLink = response.data.download_link;
+      const { track: trackName, artist: artistName, spotify_url: spotifyLink, download_link: downloadLink } = response.data;
 
       if (downloadLink) {
+        // Validate the downloadLink
+        const isUrlValid = await axios
+          .head(downloadLink)
+          .then(() => true)
+          .catch(() => false);
+
+        if (!isUrlValid) {
+          sendMessage(senderId, { text: 'Sorry, the download link is not accessible.' }, pageAccessToken);
+          return;
+        }
+
         // Upload the file to Facebook as an attachment
         const attachmentUploadUrl = `https://graph.facebook.com/v20.0/me/message_attachments?access_token=${pageAccessToken}`;
-        const attachmentResponse = await axios.post(attachmentUploadUrl, {
+        const attachmentPayload = {
           message: {
             attachment: {
               type: 'audio',
@@ -30,24 +38,33 @@ module.exports = {
               },
             },
           },
-        });
+        };
 
+        const attachmentResponse = await axios.post(attachmentUploadUrl, attachmentPayload);
         const attachmentId = attachmentResponse.data.attachment_id;
 
         // Send the uploaded attachment as a message
-        sendMessage(senderId, {
-          attachment: {
-            type: 'audio',
-            payload: {
-              attachment_id: attachmentId,
+        await sendMessage(
+          senderId,
+          {
+            attachment: {
+              type: 'audio',
+              payload: {
+                attachment_id: attachmentId,
+              },
             },
           },
-        }, pageAccessToken);
-        
+          pageAccessToken
+        );
+
         // Send additional information
-        sendMessage(senderId, {
-          text: `🎵 Song: ${trackName}\n🎤 Artist: ${artistName}\n🔗 Spotify: ${spotifyLink}`
-        }, pageAccessToken);
+        await sendMessage(
+          senderId,
+          {
+            text: `🎵 Song: ${trackName}\n🎤 Artist: ${artistName}\n🔗 Spotify: ${spotifyLink}`,
+          },
+          pageAccessToken
+        );
       } else {
         sendMessage(senderId, { text: 'Sorry, no download link found for that song.' }, pageAccessToken);
       }
@@ -55,5 +72,5 @@ module.exports = {
       console.error('Error sending audio attachment:', error.response ? error.response.data : error);
       sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
     }
-  }
+  },
 };
